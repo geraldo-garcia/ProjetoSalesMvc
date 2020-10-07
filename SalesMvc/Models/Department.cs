268adfb79f1237f7a11aed1e1d7e1d8e7c2b0bb2
Geraldo Garcia <geraldo@qmedia.com.br>
2020-10-06 22:12:44 -0300

    Other Entities and second migration

--- SalesMvc/Models/Department.cs
+++ SalesMvc/Models/Department.cs
@@ -1,4 +1,8 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
 
 namespace SalesMvc.Models
 {
@@ -6,5 +10,29 @@ namespace SalesMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        // criação dos construtores
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
